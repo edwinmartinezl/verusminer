6766dd4d59e8bd8c87dd09ea9d2b1ec473a14313
Edwin Martinez L. <34765181+edwinmartinezl@users.noreply.github.com>
2025-09-02 22:45:51 -0400

    Add files via upload
    🛠️ Verus Miner CPU - Minado Fácil para Todos
    Este repositorio te permite comenzar a minar Verus Coin (VRSC) de forma sencilla y directa usando tu CPU. Ideal para principiantes y entusiastas del minado que buscan una solución rápida sin complicaciones técnicas.
    
    🚀 ¿Qué incluye?
    hellminer.exe: ejecutable listo para usar, sin necesidad de instalación adicional.
    
    run_miner.bat: archivo de configuración predefinido para iniciar el minado con solo un doble clic.
    
    💡 Características
    Minado 100% por CPU — no necesitas GPU.
    
    Configuración rápida y sin conocimientos técnicos.
    
    Compatible con sistemas Windows.
    
    Código abierto y disponible para todos.
    
    📦 Cómo usarlo
    Descarga los archivos del repositorio.
    
    Abre start.bat y edita tu dirección de monedero Verus.
    
    Ejecuta start.bat y comienza a minar.
    
    🧠 Recomendaciones
    Asegúrate de tener una dirección de monedero Verus válida.
    
    Para mejor rendimiento, cierra otras aplicaciones mientras minas.

---
+++ run_miner.bat
@@ -0,0 +1,7 @@
+REM For more usage info: hellminer.exe --help
+
+REM Example SSL URL
+REM .\hellminer.exe -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 4
+
+.\hellminer.exe -c stratum+tcp://na.luckpool.net:3956 -u RQYkhwwtNT8EiSzxwp6iQt1ffZAjgbdTEZ.RIG1 -p x --cpu 3
+pause
